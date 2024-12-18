98fe7aed6bf924d341debe8cc83a970f4cf641ca
Yuvraj Raghuvanshi <124128352+yuvrajraghuvanshi@users.noreply.github.com>
2024-12-18 16:57:52 +0530

    updated cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: `${process.env.CLIENT_URL}/dashboard`,
     credentials: true,
   })
 );
